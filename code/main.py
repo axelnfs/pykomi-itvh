d1979ecca9449505e3599efcbf116dec1ee2084a
Axel Mosqueda <mosquedaaxel1@gmail.com>
2022-03-15 17:09:16 -0600

    15-mar-2022, conexion a db establecido

--- code/main.py
+++ code/main.py
@@ -2,15 +2,30 @@ from Cliente import Cliente
 from Producto import Producto
 from Categoria import Categoria
 from Proveedor import Proveedor
+import mysql.connector
+
+def crearPersona():
+    pass
 
 if __name__ == "__main__":
-    Cliente1 = Cliente("Adri Hiday","Herrera","Velazquez","rmoreno@mail.com")
+    #Cliente1 = Cliente("Adri Hiday","Herrera","Velazquez","rmoreno@mail.com")
     #print(vars(Cliente1))
+    #Proveedor2 = Proveedor("JOSE","SANCHEZ","OCA","jsanches@empresa.com", "chile","jempresa")
+    #Categoria1 = Categoria("Embutido")
 
-    Proveedor1 = Proveedor("Juan","Ramirez","Cordoba","jramirez@empresa.com","Espana","RamiresEspana")
-    Proveedor2 = Proveedor("JOSE","SANCHEZ","OCA","jsanches@empresa.com", "chile","jempresa")
-    Categoria1 = Categoria("Embutido")
+    #Producto1 = Producto("Jamon Espanol",20.4,10,vars(Proveedor2),vars(Categoria1))
+    #print(vars(Producto1))
 
-    Producto1 = Producto("Jamon Espanol",20.4,10,vars(Proveedor2),vars(Categoria1))
-    print(vars(Producto1))
+    mydb = mysql.connector.connect(
+        host = "localhost",
+        user = "admin",
+        password = "1234",
+        database = "TiendaElectronica"
+    )
+
+    micursor = mydb.cursor()
+    micursor.execute("show tables")
+
+    for x in micursor:
+        print (x)
 
