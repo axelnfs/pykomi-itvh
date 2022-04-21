d52c7246332735d31f9a00ee3d45080ef8ed7ca5
Axel Mosqueda <mosquedaaxel1@gmail.com>
2022-04-21 14:36:26 -0500

    Productos terminado, pendiente ventas

--- code/main.py
+++ code/main.py
@@ -1,6 +1,8 @@
 import tkinter
 from turtle import width
 from typing import Text
+
+from pyparsing import col
 import Cliente
 import Producto
 import Categoria
@@ -8,6 +10,8 @@ import Empleado
 import Proveedor
 import mysql.connector
 import time
+
+# from code.Producto import ventanaCrearProductos
 
 def ventanaBienvenida():
     ventana = tkinter.Tk()
@@ -25,14 +29,16 @@ def ventanaPrincipal():
     ventana = tkinter.Tk()
     ventana.title("Menu Principal")
     ventana.geometry("800x600")
-    botonVerEmpleados = tkinter.Button(ventana, text="Ver Empleados", width=12, height=1)
-    botonVerProveedores = tkinter.Button(ventana, text="Ver Proveedores", width=12, height=1)
+    botonEmpleados = tkinter.Button(ventana, text="EMPLEADOS", width=16, height=1)
+    botonProveedores = tkinter.Button(ventana, text="PROVEEDORES", width=16, height=1)
+    botonProductos = tkinter.Button(ventana, text="PRODUCTOS", width=16, height=1)
     nombreUltimasVentas = tkinter.Label(ventana,text="¡ULTIMAS VENTAS REGISTRADAS!")
     areaUltimasVentas = tkinter.Text(ventana, height=12,width=70)
-    botonVerEmpleados.place(x=1, y=1)
-    botonVerProveedores.place(x=100, y=1)
-    nombreUltimasVentas.place(x = 100, y=28)
-    areaUltimasVentas.place(x = 100, y = 48)
+    botonEmpleados.grid(row=0, column=0)
+    botonProveedores.grid(row=0, column=1)
+    botonProductos.grid(row=0, column=2)
+    nombreUltimasVentas.grid(row=1, column=1)
+    areaUltimasVentas.grid(row=2, column=1)
     
     ventana.mainloop()
 
@@ -44,8 +50,8 @@ if __name__ == "__main__":
     # Empleado.verEmpleados()
     # Cliente.verClientes()
     # Proveedor.verProveedores()
-    # ventanaBienvenida()
-    # ventanaPrincipal()
+    ventanaBienvenida()
+    ventanaPrincipal()
     #Cliente.verClientes()
     # Cliente.ventanaCliente()
     # Cliente.ventanaCrearCliente()
@@ -54,6 +60,9 @@ if __name__ == "__main__":
     # Empleado.ventanaEliminarEmpleado()
     # Proveedor.ventanaCrearProveedores()
     # Proveedor.ventanaEliminarProveedor()
-    Proveedor.ventanaProveedores()
+    # Proveedor.ventanaProveedores()
+    # Producto.ventanaCrearProductos()
+    # Producto.ventanaCrearProductos()
+    Producto.ventanaProductos()
 
 
